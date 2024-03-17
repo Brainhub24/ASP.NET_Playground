4e9a501c6e900583bab9ad24eff504822d82d50d
Jan Gebser <600613.m6@gmail.com>
2024-03-17 10:21:22 +0100

    First run #test

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,2 +1,21 @@
 ﻿using System;
 
+namespace YourNamespace
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Check if the user-agent indicates a bot
+            if (Request.UserAgent != null && (Request.UserAgent.Contains("Googlebot") || Request.UserAgent.Contains("Bingbot")))
+            {
+                // Redirect bots to another site
+                Response.Redirect("https://playground.rapidsource.eu/test/", true);
+            }
+            else
+            {
+                // Continue serving the regular landing page
+            }
+        }
+    }
+}
