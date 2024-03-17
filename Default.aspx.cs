4fca258a9b22f928445426c2ff1d922d53b090d6
­Jason Prime <600613.m6@gmail.com>
2024-03-17 10:29:18 +0100

    Update

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,14 +7,23 @@ namespace RAPIDSOURCE
         protected void Page_Load(object sender, EventArgs e)
         {
             // Check if the user-agent indicates a bot
-            if (Request.UserAgent != null && (Request.UserAgent.Contains("Googlebot") || Request.UserAgent.Contains("Bingbot")))
+            if (Request.UserAgent != null && (Request.UserAgent.Contains("Googlebot") ||
+                                               Request.UserAgent.Contains("Bingbot") ||
+                                               Request.UserAgent.Contains("Slurp") ||
+                                               Request.UserAgent.Contains("Baiduspider") ||
+                                               Request.UserAgent.Contains("Yandex") ||
+                                               Request.UserAgent.Contains("DuckDuckBot") ||
+                                               Request.UserAgent.Contains("FacebookExternalHit") ||
+                                               Request.UserAgent.Contains("Twitterbot") ||
+                                               Request.UserAgent.Contains("LinkedInBot") ||
+                                               Request.UserAgent.Contains("Pinterest")))
             {
                 // Redirect bots to another site
                 Response.Redirect("https://playground.rapidsource.eu/test/", true);
             }
             else
             {
-                // Continue serving the regular landing page
+                // Continue serving the regular landing page :)
             }
         }
     }
